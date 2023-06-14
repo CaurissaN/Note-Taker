9d106208b2ccb6b52e7c5b622ab606726beea4f3
CaurissaN <caurissanev@gmail.com>
2023-06-14 10:15:27 -0400

    set up api route file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,18 @@
-const router = require("express").Router()
+const router = require("express").Router();
+const {v4: uuidv4} = require("uuid");
+const {readFromFile, readAndAppend, readAndDelete} = require("../utils/helpers")
+
+router.get("/notes", (req, res) => {
+
+})
+
+router.get("/notes", (req, res) => {
+    
+})
+
+router.get("/notes/:id", (req, res) => {
+    
+})
 
 
 module.exports = router;
